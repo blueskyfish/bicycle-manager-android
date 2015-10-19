7cb4f1418070d42a0c6e3832994ea70944509a16
BlueSkyFish <blueskyfish@kirchnerei.de>
2015-10-19 13:56:54 +0200

    #12: Update the license description

--- bicycle-manager/src/main/java/de/kirchnerei/bicycle/battery/BatteryDetailFragment.java
+++ bicycle-manager/src/main/java/de/kirchnerei/bicycle/battery/BatteryDetailFragment.java
@@ -1,3 +1,9 @@
+/*
+ * The MIT License (MIT)
+ * Copyright (c) 2015 BlueSkyFish
+ *
+ * bicycle-manager-android - https://github.com/blueskyfish/bicycle-manager-android.git
+ */
 package de.kirchnerei.bicycle.battery;
 
 
